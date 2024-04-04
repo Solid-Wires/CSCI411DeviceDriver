8185be20a1fb47a121b473e346c70d214cae0324
Solid Wires <solid.wires@gmail.com>
2024-04-04 11:16:01 -0400

    Prepared the device reader

---
+++ Makefile
@@ -0,0 +1,1 @@
+g++ ./src/DeviceReader.cc -o ./bin/DeviceReader
---
+++ src/DeviceReader.cc
@@ -0,0 +1,22 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main() 
+{
+    // Open from the device file we just made
+    //  O_NONBLOCK is a flag I haven't seen before. It's a Linux exclusive flag specifically for when
+    //  we just want to open something and nothing more. RDWR is explicitly stated here though.
+    int file = open("/dev/memory", O_RDWR | O_NONBLOCK);
+
+    // Write into that device file
+    ssize_t writeTest = write(file, "Jared", 1);
+    char bufStatus[1];
+
+    // Read from that device file
+    ssize_t readTest = read(file, bufStatus, 1);
+
+    // Close the device file
+    close(file);
+    printf("%s\n", bufStatus);
+}
