7612650e6c6f7b5a6c101b79cf4f175003fd9243
Solid Wires <solid.wires@gmail.com>
2024-04-04 13:37:52 -0400

    Forget it

--- src/DeviceReader.cc
+++ src/DeviceReader.cc
@@ -1,7 +1,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <string>
 #include <cstring>
 #include <iostream>
 
@@ -12,8 +11,7 @@ int main()
     //  we just want to open something and nothing more. RDWR is explicitly stated here though.
     int file = open("/dev/memory", O_RDWR | O_NONBLOCK);
 
-    const char* toWrite;
-    std::getline(std::cin, toWrite);
+    const char* toWrite = "Jared";
 
     // Write into that device file
     ssize_t writeTest = write(file, toWrite, std::strlen(toWrite));
