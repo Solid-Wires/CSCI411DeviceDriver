305d1c5a6b7a32cb4592d0182f9db9f55de86e1c
Solid Wires <solid.wires@gmail.com>
2024-04-04 00:56:03 -0400

    began the hello driver

---
+++ modules/helloworld/hello.c
@@ -0,0 +1,18 @@
+#include <linux/init.h>  /* module_init and module_exit macros */
+#include <linux/module.h>  /* links to kernel version */
+#include <linux/kernel.h> /* printk() */
+
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR(“Marguerite Doman”)
+
+static int hello_init(void) {
+  printk("<1> Hello world!\n");
+  return 0;
+}
+
+static void hello_exit(void) {
+  printk("<1> Bye, cruel world\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
